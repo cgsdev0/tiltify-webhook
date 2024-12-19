7b06e13611eff91cfbb956e5c0df3c603531f7f9
Sarah Schulte <git@sarah.engineer>
2024-12-19 01:30:21 -0800

    argh

--- index.ts
+++ index.ts
@@ -8,12 +8,12 @@ const app = express();
 
 const threshold = process.argv[2] || 1;
 
-app.use(bodyParser.text());
+app.use(bodyParser.text({ type: "*/*" }));
 
 const hmacMiddleware = (req: any, res: any, next: any) => {
   const headers = req.headers;
   const timestamp = req.headers["x-tiltify-timestamp"] || "";
-  const body = JSON.stringify(req.body);
+  const body = req.body;
   const payload = `${timestamp}.${body}`;
   console.log(payload);
   const digest = crypto
